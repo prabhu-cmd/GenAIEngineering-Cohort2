8d0092a5ee9f35d4325b62cc8fe4fb0f515b23da
Prabhu K <syz.prabu@gmail.com>
2025-06-15 19:35:04 +0530

    Added execution results and modifications to BaseCamp2 Day1 code

--- BaseCamp2/Day_1/Use_Module.py
+++ BaseCamp2/Day_1/Use_Module.py
@@ -4,11 +4,11 @@ Import can be done entire module Or part of it.
 """
 
 # Overall module import
-import Data_Processing
+# import Data_Processing
 
-res = Data_Processing.Process_Data (23.8, 13.5)
-print (res)
-print (Data_Processing.Module_Version)
+# res = Data_Processing.Process_Data (23.8, 13.5)
+# print (res)
+# print (Data_Processing.Module_Version)
 
 # ------------------------------------
 
@@ -20,6 +20,6 @@ print (Data_Processing.Module_Version)
 # ------------------------------------
 
 # # Import from a package
-# from Data_Pack.Data_Processing import Print_Data
+from Data_Pack.Data_Processing import Print_Data
 
-# Print_Data (34, 987)
+Print_Data (34, 987)
